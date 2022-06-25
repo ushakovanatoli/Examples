08bee95ee178f08f745c42d324c2f3278a2bdfa0
anatoli <ushakovanatoli3@mail.ru>
2022-06-25 17:16:09 +0300

    Example 005 if else

---
+++ Example_005_ConditionifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("УРА, ЭТО ЖЕ МАША!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
